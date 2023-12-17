03dba32167e7f997568b83e4d1213b1d6de16e0e
900119 <anggun.perpatih@bni.co.id>
2023-12-17 11:15:33 +0700

    First Commit Front End Authentication and Registration using Angular 10

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from '@angular/core';
+
+import { User } from '@app/_models';
+import { AccountService } from '@app/_services';
+import { first } from 'rxjs/operators';
+
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    users = null;
+
+    constructor(private accountService: AccountService) {}
+
+    ngOnInit() {
+        this.accountService.getAll()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
+
+    deleteUser(id: string) {
+        const user = this.users.find(x => x.id === id);
+        user.isDeleting = true;
+        this.accountService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.users = this.users.filter(x => x.id !== id));
+    } 
+}
